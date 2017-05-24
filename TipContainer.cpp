0555f90fd3dc7673dcf1ccb668702c0a00713cb7
Geralt <parvezl123@gmail.com>
2017-05-24 23:43:32 +0200

    zmiana zmiennej w zapisywaniu

--- TipContainer.cpp
+++ TipContainer.cpp
@@ -10,7 +10,7 @@ int loadTips(TipContainer& cont, const char* fileName)
 	if (!reader) return -1; //error while opening file
 
 	char type;
-	int dir;
+	direction dir;
 	size_t n;
 
 
@@ -21,22 +21,22 @@ int loadTips(TipContainer& cont, const char* fileName)
 		if (type == 'T')
 		{
 			reader.read((char*) &dir, sizeof(direction));
-			cont.add(std::unique_ptr<Tip>(new Turn(static_cast<direction>(dir))));
+			cont.add(std::unique_ptr<Tip>(new Turn(dir)));
 		}
 		else if (type == 'D')
 		{
 			reader.read((char*) &dir, sizeof(direction));
-			cont.add(std::unique_ptr<Tip>(new Destination(static_cast<direction>(dir))));
+			cont.add(std::unique_ptr<Tip>(new Destination(dir)));
 		}
 		else if (type == 'F')
 		{
 			reader.read((char*) &n, sizeof(size_t));
-			cont.add(std::unique_ptr<Tip>(new Forward(static_cast<direction>(n))));
+			cont.add(std::unique_ptr<Tip>(new Forward(n)));
 		}
 		else if (type == 'E')
 		{
 			reader.read((char*)&n, sizeof(size_t));
-			cont.add(std::unique_ptr<Tip>(new ExitRamp(static_cast<direction>(n))));
+			cont.add(std::unique_ptr<Tip>(new ExitRamp(n)));
 		}
 		else return 1; // error while reading
 	}
