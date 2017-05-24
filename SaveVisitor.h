1a404ccfd000ad33b103e3547451225e554c383a
Geralt <parvezl123@gmail.com>
2017-05-24 23:39:02 +0200

    zamiana zapisu

--- SaveVisitor.h
+++ SaveVisitor.h
@@ -5,15 +5,36 @@
 
 struct SaveVisitor : public Visitor
 {
-	SaveVisitor(const char* fileName) :  out(fileName, std::ios::out | std::ios::binary) {}
+	SaveVisitor(const char* fileName) : out(fileName, std::ios::out | std::ios::binary) {}
 	~SaveVisitor() { out.close(); }
 
 private:
 	std::ofstream out;
 public:
-	virtual void visit_Turn(const Turn& tip) { out << "T" << tip.dir; }
-	virtual void visit_Forward(const Forward& tip) {out << "F" << tip.n; }
-	virtual void visit_ExitRamp(const ExitRamp& tip) {out << "E" << tip.n; }
-	virtual void visit_Destination(const Destination &tip) {out << "D" << tip.dir; }
+	virtual void visit_Turn(const Turn& tip) { out << "T"; out.write((char*)& tip.dir, sizeof(direction)); }
+	virtual void visit_Forward(const Forward& tip) { out << "F";  out.write((char*)& tip.n, sizeof(size_t)); }
+	virtual void visit_ExitRamp(const ExitRamp& tip) { out << "E"; out.write((char*)& tip.n, sizeof(size_t)); }
+	virtual void visit_Destination(const Destination &tip) { out << "D"; out.write((char*)& tip.dir, sizeof(direction)); }
 };
 
+
+
+//
+//#pragma once
+//#include "Tips.h"
+//#include <iostream>
+//#include <fstream>
+//
+//struct SaveVisitor : public Visitor
+//{
+//	SaveVisitor(const char* fileName) : out(fileName, std::ios::out | std::ios::binary) {}
+//	~SaveVisitor() { out.close(); }
+//
+//private:
+//	std::ofstream out;
+//public:
+//	virtual void visit_Turn(const Turn& tip) { out << "T" << tip.dir; }
+//	virtual void visit_Forward(const Forward& tip) { out << "F" << tip.n; }
+//	virtual void visit_ExitRamp(const ExitRamp& tip) { out << "E" << tip.n; }
+//	virtual void visit_Destination(const Destination &tip) { out << "D" << tip.dir; }
+//};
--- TipContainer.cpp
+++ TipContainer.cpp
@@ -1,5 +1,9 @@
 #include "TipContainer.h"
 
+
+
+
+
 int loadTips(TipContainer& cont, const char* fileName)
 {
 	std::ifstream reader(fileName);
@@ -10,32 +14,90 @@ int loadTips(TipContainer& cont, const char* fileName)
 	size_t n;
 
 
-	while (!reader.eof())
+	while (reader >> type)
 	{
-		reader >> type;
+
+
 		if (type == 'T')
 		{
-			reader >> dir;
-			//	reader dir;
+			reader.read((char*) &dir, sizeof(direction));
 			cont.add(std::unique_ptr<Tip>(new Turn(static_cast<direction>(dir))));
 		}
 		else if (type == 'D')
 		{
-			reader >> dir;
-			//	reader dir;
+			reader.read((char*) &dir, sizeof(direction));
 			cont.add(std::unique_ptr<Tip>(new Destination(static_cast<direction>(dir))));
 		}
 		else if (type == 'F')
 		{
-			reader >> n;
-			cont.add(std::unique_ptr<Tip>(new Forward(n)));
+			reader.read((char*) &n, sizeof(size_t));
+			cont.add(std::unique_ptr<Tip>(new Forward(static_cast<direction>(n))));
 		}
 		else if (type == 'E')
 		{
-			reader >> n;
-			cont.add(std::unique_ptr<Tip>(new ExitRamp(n)));
+			reader.read((char*)&n, sizeof(size_t));
+			cont.add(std::unique_ptr<Tip>(new ExitRamp(static_cast<direction>(n))));
 		}
 		else return 1; // error while reading
 	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//int loadTips(TipContainer& cont, const char* fileName)
+//{
+//	std::ifstream reader(fileName);
+//	if (!reader) return -1; //error while opening file
+//
+//	char type;
+//	int dir;
+//	size_t n;
+//
+//
+//	while (!reader.eof())
+//	{
+//		reader >> type;
+//		if (type == 'T')
+//		{
+//			reader >> dir;
+//			//	reader dir;
+//			cont.add(std::unique_ptr<Tip>(new Turn(static_cast<direction>(dir))));
+//		}
+//		else if (type == 'D')
+//		{
+//			reader >> dir;
+//			//	reader dir;
+//			cont.add(std::unique_ptr<Tip>(new Destination(static_cast<direction>(dir))));
+//		}
+//		else if (type == 'F')
+//		{
+//			reader >> n;
+//			cont.add(std::unique_ptr<Tip>(new Forward(n)));
+//		}
+//		else if (type == 'E')
+//		{
+//			reader >> n;
+//			cont.add(std::unique_ptr<Tip>(new ExitRamp(n)));
+//		}
+//		else return 1; // error while reading
+//	}
+//	return 0;
+//}
